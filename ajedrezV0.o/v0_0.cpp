6c5abfd9bf6ef4cf865f86005130353593a19e22
jriveortiz <131053298+jriveortiz@users.noreply.github.com>
2024-04-07 17:35:04 +0200

    nuevos .h y .cpp

--- ajedrezV0.o/v0_0.cpp
+++ ajedrezV0.o/v0_0.cpp
@@ -1,66 +1,8 @@
 #include <iostream>
 #include <vector>
+#include "tablero.h"
 
 // Definición de clases
-class Pieza {
-public:
-    enum class Tipo { Peon, Caballo, Alfil, Torre, Rey, Reina };
-    enum class Color { Blanca, Negra };
-
-    //Constructor
-    Pieza(Tipo tipo, Color color, int fila, int col) : tipo(tipo), color(color), fila(fila), col(col) {}
-
-    // Métodos para obtener información sobre la pieza
-    Tipo getTipo() const { return tipo; }
-    Color getColor() const { return color; }
-    int getFila() const { return fila; }
-    int getCol() const { return col; }
-
-    // Métodos para mover la pieza y otras operaciones relacionadas
-    void mover(int nuevafila, int nuevacol) {
-        // Implementación del movimiento de la pieza
-    }
-
-private:
-    Tipo tipo;
-    Color color;
-    int fila;
-    int col;
-};
-
-class Tablero {
-public:
-    Tablero() {
-        // Inicializar el tablero con piezas en las posiciones iniciales
-        casillas.resize(5, std::vector<Pieza*>(5, nullptr)); // Tablero de 8x8
-        // Aquí debes inicializar las casillas con las piezas en su posición inicial
-    }
-
-    // Métodos para mostrar el tablero, realizar movimientos, etc.
-    void mostrar() const {
-        // Implementación para mostrar el tablero en la consola
-    }
-
-    Pieza* obtenerPiezaEnPosicion(int fila, int col) const {
-        return casillas[fila][col];
-    }
-
-    // Método para comprobar si una casilla está ocupada por una pieza
-    bool estaOcupada(int fila, int col) const {
-        return casillas[fila][col] != nullptr;
-    }
-
-    // Método para comprobar si una casilla está ocupada por una pieza de un color específico
-    bool estaOcupadaPorColor(int fila, int col, Piza::Color color) const {
-        if (casillas[fila][col] != nullptr) {e
-            return casillas[fila][col]->getColor() == color;
-        }
-        return false;
-    }
-
-private:
-    std::vector<std::vector<Pieza*>> casillas; // Representación del tablero
-};
 
 class Juego {
 public:
