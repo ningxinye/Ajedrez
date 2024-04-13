bfa82f470b3f57be204a7e4ea376292da51cbe8f
ningxinye <Ynx1316290320@gmail.com>
2024-04-13 22:31:57 +0200

    XX

---
+++ Ajedrez/src/Ajedrez.cpp
@@ -0,0 +1,40 @@
+<<<<<<< Updated upstream:Ajedrez/Ajedrez.cpp
+#include "Ajedrez.h"
+
+using namespace std;
+
+void Ajedrez::dibuja()
+{
+	gluLookAt(4, 4, 12,  // posicion del ojo
+		4, 4, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)  
+	
+	tablero.dibuja();
+
+
+
+}
+
+void Ajedrez::inicializa()
+{
+	tablero.posicionInicial();
+=======
+#include "Ajedrez.h"
+
+using namespace std;
+
+void Ajedrez::dibuja()
+{
+	gluLookAt(4, 4, 12,  // posicion del ojo
+		4, 4, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)  
+	tablero.dibuja();
+	tablero.posicionInicial();
+
+}
+
+void Ajedrez::inicializa()
+{
+	//soy irene
+>>>>>>> Stashed changes:Ajedrez/src/Ajedrez.cpp
+}
---
+++ Ajedrez/src/Ajedrez.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Tablero.h"
+#include "freeglut.h"
+//para gestionar el juego, por lo tanto, hace falta organizar dos cosas:
+//gr¨¢fico del juego y l¨®gico del juego
+class Ajedrez
+{
+private:
+	Tablero tablero;
+
+
+public:
+	//gr¨¢fica
+	void dibuja();
+	void inicializa();
+
+	//l¨®gico
+
+
+
+};
