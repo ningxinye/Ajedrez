6dbae838764ed9605c87ac3c4789375fe035cb2b
ningxinye <Ynx1316290320@gmail.com>
2024-04-19 23:46:26 +0200

    XX

--- Ajedrez/src/Ajedrez.cpp
+++ Ajedrez/src/Ajedrez.cpp
@@ -9,12 +9,13 @@ void Ajedrez::dibuja()
 		4, 4, 0.0,      // hacia que punto mira  (0,0,0) 
 		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)  
 	tablero.dibuja();
-	tablero.posicionInicial();
+
 
 }
 
 void Ajedrez::inicializa()
 {
+	tablero.posicionInicial();
 	//soy irene
 
 }
