163c7162217d466c50fbd7a71f8db14ce960f06c
SergioCantalejoVillasante <80417134+SergioCantalejoVillasante@users.noreply.github.com>
2024-06-06 17:58:19 +0200

    Update alfil.h

--- Ajedrez/src/alfil.h
+++ Ajedrez/src/alfil.h
@@ -5,7 +5,7 @@
 class alfil
 {
 public:
-    // Función para verificar los movimientos posibles de un pe¨®n
+    // Función para verificar los movimientos posibles de un alfil
     static bool SePuedeMover(const Casilla& origen, const Casilla& destino, Pieza* casillas[5][5]);
 
 };
