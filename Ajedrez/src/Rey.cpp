f6e0a29948b8ea01f5bf211053be915150d7f81c
QINGYUN-XU <80351524+QINGYUN-XU@users.noreply.github.com>
2024-06-07 01:26:41 +0200

    Add files via upload

--- Ajedrez/src/Rey.cpp
+++ Ajedrez/src/Rey.cpp
@@ -2,19 +2,50 @@
 #include <iostream>
 #include <cmath>
 
-bool Rey::esMovimientoValido(const Casilla& origen, const Casilla& destino, Pieza* casillas[5][5]) {
-    std::cout << "Comprobando movimiento del rey: desde (" << origen.f << ", " << origen.c << ") hasta (" << destino.f << ", " << destino.c << ")\n";
+bool Rey::esMovimientoValido(const Casilla& origen, const Casilla& destino, Pieza* casillas[5][5])
+{
+    std::cout << "Comprobando movimiento del rey desde (" << origen.f << ", " << origen.c << ") hasta (" << destino.f << ", " << destino.c << ")\n";
 
-    int diferenciaDeFilas = abs(destino.f - origen.f);
-    int diferenciaDeColumnas = abs(destino.c - origen.c);
-    //Movimiento del rey
+    int diferenciaDeFilas = destino.f - origen.f;
+    int diferenciaDeColumnas = destino.c - origen.c;
 
-    if (diferenciaDeFilas <= 1 && diferenciaDeColumnas <= 1) {
+	if (casillas[origen.f][origen.c]->getColor() == Blanca)
+	{
 
-        if (casillas[destino.f][destino.c]->getTipo() == No_pieza || casillas[destino.f][destino.c]->getColor() != casillas[origen.f][origen.c]->getColor()) {
-            return true;
-        }
-    }
+		std::cout << "Intento de movimiento de una pieza blanca.\n";
 
-    return false;
+		// Movimiento hacia adelante
+		if (diferenciaDeFilas <= 1 && diferenciaDeColumnas <= 1)
+		{
+			std::cout << "Movimiento en la misma columna. Cambio de fila: " << diferenciaDeFilas << "\n";
+			if (casillas[destino.f][destino.c]->getTipo() == No_pieza) {
+				return true;
+			}
+			if (casillas[destino.f][destino.c]->getColor() != casillas[origen.f][origen.c]->getColor()) {
+				return true;
+			}
+		}
+	}
+	else if (casillas[origen.f][origen.c]->getColor() == Negra) 
+	{
+		std::cout << "Intento de movimiento de una pieza negra.\n";
+		if (diferenciaDeFilas <= 1 && diferenciaDeColumnas <= 1) 
+		{
+			std::cout << "Movimiento en la misma columna. Cambio de fila: " << diferenciaDeFilas << "\n";
+			if (casillas[destino.f][destino.c]->getTipo() == No_pieza) 
+			{
+				return true;
+			}
+			if (casillas[destino.f][destino.c]->getColor() != casillas[origen.f][origen.c]->getColor())
+			{
+				return true;
+			}
+		}
+
+		return false;
+
+
+	}
+
+
 }
